c34330c91a8712bcf6220fe1ed1449a6a05828fc
Zhengchao Yu <cyu8017@users.noreply.github.com>
2016-12-20 14:15:12 -0500

    Update TestCircle2D.java

--- Assignment4/TestCircle2D.java
+++ Assignment4/TestCircle2D.java
@@ -17,8 +17,8 @@
 
 public class TestCircle2D {
     
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
+	    
 	// -- Create c1, c2, c3 Circle2D objects
 	Circle2D c1 = new Circle2D(2, 2, 5.5, "YELLOW");
 	Circle2D c2 = new Circle2D(4, 5, 10.5, "BLUE");
@@ -27,38 +27,52 @@ public class TestCircle2D {
 	// -- Test case for the circle with Circle2D(2, 2, 5.5, "YELLOW");
 	System.out.println("Test result for c1's getArea() method is " +
 			   (c1.getArea() == 95.03317777109125 ? "Passed" : "Failed"));
+	    
 	System.out.println("Test result for c1's getPerimeter() method is " +
 			   (c1.getPerimeter() == 34.55751918948772 ? "Passed" : "Failed"));
+	    
 	System.out.println("Test result for c1's contains() method with (3, 3) " +
 			   "where x=3 and y=3 is " + 
 			   (c1.contains(3, 3) == true ? "Passed" : "Failed"));
+	    
 	System.out.println("Test result for c1 contains c2 is " +
 			   (c1.contains(c2) == false ? "Passed" : "Failed"));
+	    
 	System.out.println("Test result for c1 contains c3 is " +
 			   (c1.contains(c3) == true ? "Passed" : "Failed"));
+	    
 	System.out.println("Test result for c1 overlaps c2 is " +
 			   (c1.overlaps(c2) == true ? "Passed" : "Failed"));
+	    
 	System.out.println("Test result for c1 overlaps c3 is " +
 			   (c1.overlaps(c3) == true ? "Passed" : "Failed"));
+	    
 	System.out.println("Test result for c1's getColor() method is " +
 			   (c1.getColor().equals("YELLOW") ? "Passed" : "Failed"));
 	
 	// -- Test case for the circle with Circle2D(4, 5, 10.5, "BLUE");
 	System.out.println("Test result for c2's getArea() method is " +
 			   (c2.getArea() == 346.3605900582747 ? "Passed" : "Failed"));
+	    
 	System.out.println("Test result for c2's getPerimeter() method is " +
 			   (c2.getPerimeter() == 65.97344572538566 ? "Passed" : "Failed"));
+	    
 	System.out.println("Test result for c2's contains() method with (2, 20) " +
 			   "where x=2 and y=20 is " + 
 			   (c2.contains(2, 20) == false ? "Passed" : "Failed"));
+	    
 	System.out.println("Test result for c2 contains c1 is " +
 			   (c2.contains(c1) == true ? "Passed" : "Failed"));
+	    
 	System.out.println("Test result for c2 contains c3 is " +
 			   (c2.contains(c3) == true ? "Passed" : "Failed"));
+	    
 	System.out.println("Test result for c2 overlaps c1 is " +
 			   (c2.overlaps(c1) == true ? "Passed" : "Failed"));
+	    
 	System.out.println("Test result for c2 overlaps c3 is " +
 			   (c2.overlaps(c3) == true ? "Passed" : "Failed"));
+	    
 	System.out.println("Test result for c2's getColor() method is " +
 			   (c2.getColor().equals("BLUE") ? "Passed" : "Failed"));
 	
